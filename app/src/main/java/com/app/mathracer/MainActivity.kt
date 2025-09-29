82586568354d704097a5ec235c58d3abfcb3ad9c
esmepantano <pantanoesmeralda@gmail.com>
2025-09-28 21:17:42 -0300

    screen waiting opponent

--- app/src/main/java/com/app/mathracer/MainActivity.kt
+++ app/src/main/java/com/app/mathracer/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.app.mathracer.ui.WaitingOpponentScreen
 import com.app.mathracer.ui.theme.MathRacerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +20,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MathRacerTheme {
+                WaitingOpponentScreen()
+                /*
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
+
+                 */
             }
         }
     }
