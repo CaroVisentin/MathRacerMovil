a5343e36bac6b0f97675e0fc9b6fb00e28a26d47
CaroVisentin <carolinaaylen26@gmail.com>
2025-11-24 13:41:47 -0300

    trae datos de nivel solo para el mundo actual

--- app/src/main/java/com/app/mathracer/ui/screens/worlds/viewmodel/WorldsViewModel.kt
+++ app/src/main/java/com/app/mathracer/ui/screens/worlds/viewmodel/WorldsViewModel.kt
@@ -3,6 +3,7 @@ package com.app.mathracer.ui.screens.worlds.viewmodel
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.app.mathracer.data.CurrentUser
 import com.app.mathracer.data.model.WorldDto
 import com.app.mathracer.data.repository.LevelsRemoteRepository
 import com.app.mathracer.data.model.Worlds
@@ -32,15 +33,19 @@ class WorldsViewModel : ViewModel() {
                     val progressMap = mutableMapOf<Int, Pair<Int, Int>>()
                     for (w in fetchedWorlds) {
                         try {
-                            val levelsResp = LevelsRemoteRepository.getLevels(w.id)
-                            if (levelsResp.isSuccessful) {
-                                val levelsBody = levelsResp.body()
-                                val total = levelsBody?.levels?.size ?: 15
-                                val lastCompletedId = levelsBody?.lastCompletedLevelId ?: 0
-                                val completed = levelsBody?.levels?.count { it.id <= lastCompletedId } ?: 0
-                                progressMap[w.id] = Pair(completed, total)
-                            } else {
-                                progressMap[w.id] = Pair(0, 15)
+                            Log.d("WorldsViewModel", "Fetching levels for world ${w}")
+                            if(w.id == (worlds?.lastAvailableWorldId ?: 0)) {
+                                val levelsResp = LevelsRemoteRepository.getLevels(w.id)
+                                if (levelsResp.isSuccessful) {
+                                    val levelsBody = levelsResp.body()
+                                    val total = levelsBody?.levels?.size ?: 15
+                                    val lastCompletedId = levelsBody?.lastCompletedLevelId ?: 0
+                                    val completed =
+                                        levelsBody?.levels?.count { it.id <= lastCompletedId } ?: 0
+                                    progressMap[w.id] = Pair(completed, total)
+                                } else {
+                                    progressMap[w.id] = Pair(0, 15)
+                                }
                             }
                         } catch (e: Exception) {
                             android.util.Log.e("WorldsViewModel", "Error fetching levels for world ${w.id}", e)
